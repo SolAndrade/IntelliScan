ca1dd624c4520cf8472dba7110b90801e6600b46
Sol Andrade <solnicandrade@gmail.com>
2024-11-09 21:38:37 +0200

    .

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiKey: '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'
+    openAiApiKey: ''
   };
