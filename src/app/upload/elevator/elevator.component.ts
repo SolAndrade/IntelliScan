45d37001d95167352c2c5fc4bf069b976800330b
Sol Andrade <solnicandrade@gmail.com>
2024-11-09 21:03:50 +0200

    elevator

--- src/app/upload/elevator/elevator.component.ts
+++ src/app/upload/elevator/elevator.component.ts
@@ -1,174 +1,80 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import * as THREE from 'three';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-elevator',
   templateUrl: './elevator.component.html',
   styleUrls: ['./elevator.component.css'],
-  providers: [{ provide: Window, useValue: window }],
 })
 export class ElevatorComponent implements AfterViewInit {
-  private isDragging = false;
-  private previousMousePosition = { x: 0, y: 0 };
   private camera!: THREE.PerspectiveCamera;
+  private scene!: THREE.Scene;
+  private renderer!: THREE.WebGLRenderer;
+  private rotateDirection: 'left' | 'right' | null = null;
+  faArrowLeft = faArrowLeft;
+  faArrowRight = faArrowRight;
 
   constructor() {}
 
   ngAfterViewInit() {
-    const width = window.innerWidth;
-    const height = window.innerHeight;
+    const container = document.getElementById('three-container');
+    const width = container ? container.clientWidth : window.innerWidth;
+    const height = container ? container.clientHeight : window.innerHeight;
 
     // Initialize camera
     this.camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 10);
-    this.camera.position.z = 2; // Start with a farther position to enable zooming
+    this.camera.position.z = 2;
 
     // Initialize scene
-    const scene = new THREE.Scene();
+    this.scene = new THREE.Scene();
 
     // Create geometry and material
-    const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
     const material = new THREE.MeshNormalMaterial();
-    const mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
+
+    const metalMaterial = new THREE.MeshStandardMaterial({
+      color: 0xb4b4b4, // Grey color
+      metalness: 0.7, // High metalness for a metallic appearance
+      roughness: 0.3, // Low roughness for a somewhat reflective surface
+    });
+
+    const light = new THREE.PointLight(0xffffff, 2, 7);
+    light.position.set(1, 1, 2);
+    this.scene.add(light);
+
+    const leftDoor = new THREE.BoxGeometry(0.55, 1.5, 1.1);
+    const meshLeftDoor = new THREE.Mesh(leftDoor, metalMaterial);
+    meshLeftDoor.position.set(-0.29, 0, 0);
+    this.scene.add(meshLeftDoor);
+
+    const rightDoor = new THREE.BoxGeometry(0.55, 1.5, 1.1);
+    const meshRightDoor = new THREE.Mesh(rightDoor, metalMaterial);
+    meshRightDoor.position.set(0.29, 0, 0);
+    this.scene.add(meshRightDoor);
 
     // Initialize renderer with transparency
-    const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-    renderer.setSize(width, height);
-    renderer.setClearColor(0x000000, 0); // Transparent background
-    renderer.render(scene, this.camera);
+    this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+    this.renderer.setSize(width, height);
+    this.renderer.setClearColor(0x000000, 0); // Transparent background
 
     // Append the renderer's DOM element to the component's container
-    const container = document.getElementById('three-container');
     if (container) {
-      container.style.width = '100vw'; // Full width
-      container.style.height = '100vh'; // Full height
-      container.appendChild(renderer.domElement);
-    }
-
-    // Event listeners for mouse interaction
-    renderer.domElement.addEventListener('mousedown', (event) =>
-      this.onMouseDown(event)
-    );
-    renderer.domElement.addEventListener('mousemove', (event) =>
-      this.onMouseMove(event, mesh, renderer, scene)
-    );
-    renderer.domElement.addEventListener('mouseup', () => this.onMouseUp());
-    renderer.domElement.addEventListener('wheel', (event) =>
-      this.onMouseWheel(event, renderer, scene)
-    );
-
-    // Touch events
-    renderer.domElement.addEventListener('touchstart', (event) =>
-      this.onTouchStart(event)
-    );
-    renderer.domElement.addEventListener('touchmove', (event) =>
-      this.onTouchMove(event, scene)
-    );
-    renderer.domElement.addEventListener('touchend', () => this.onTouchEnd());
-
-    // Animation function to re-render scene
-    const animate = () => {
-      renderer.render(scene, this.camera);
-      requestAnimationFrame(animate);
-    };
-    animate();
-  }
-
-  // Mouse events
-  private onMouseDown(event: MouseEvent) {
-    this.isDragging = true;
-    this.previousMousePosition = {
-      x: event.clientX,
-      y: event.clientY,
-    };
-  }
-
-  private onMouseMove(
-    event: MouseEvent,
-    mesh: THREE.Mesh,
-    renderer: THREE.WebGLRenderer,
-    scene: THREE.Scene
-  ) {
-    if (this.isDragging) {
-      const deltaMove = {
-        x: event.clientX - this.previousMousePosition.x,
-        y: event.clientY - this.previousMousePosition.y,
-      };
-
-      const rotationX = scene.rotation.x + deltaMove.y * 0.01;
-      const rotationY = scene.rotation.y + deltaMove.x * 0.01;
-
-      // Limit rotation to prevent flipping upside down
-      scene.rotation.x = Math.max(
-        -Math.PI / 2,
-        Math.min(Math.PI / 2, rotationX)
-      );
-      scene.rotation.y = rotationY;
-
-      this.previousMousePosition = {
-        x: event.clientX,
-        y: event.clientY,
-      };
+      container.appendChild(this.renderer.domElement);
+      this.renderer.render(this.scene, this.camera);
     }
   }
 
-  private onMouseUp() {
-    this.isDragging = false;
+  rotateLeft() {
+    this.scene.rotation.y -= 0.1;
+    this.renderer.render(this.scene, this.camera);
   }
 
-  // Touch events
-  private onTouchStart(event: TouchEvent) {
-    if (event.touches.length === 1) {
-      this.isDragging = true;
-      const touch = event.touches[0];
-      this.previousMousePosition = {
-        x: touch.clientX,
-        y: touch.clientY,
-      };
-    }
+  rotateRight() {
+    this.scene.rotation.y += 0.1;
+    this.renderer.render(this.scene, this.camera);
   }
 
-  private onTouchMove(event: TouchEvent, scene: THREE.Scene) {
-    if (this.isDragging && event.touches.length === 1) {
-      const touch = event.touches[0];
-      const deltaMove = {
-        x: touch.clientX - this.previousMousePosition.x,
-        y: touch.clientY - this.previousMousePosition.y,
-      };
-
-      const rotationX = scene.rotation.x + deltaMove.y * 0.01;
-      const rotationY = scene.rotation.y + deltaMove.x * 0.01;
-
-      // Limit rotation to prevent flipping upside down
-      scene.rotation.x = Math.max(
-        -Math.PI / 2,
-        Math.min(Math.PI / 2, rotationX)
-      );
-      scene.rotation.y = rotationY;
-
-      this.previousMousePosition = {
-        x: touch.clientX,
-        y: touch.clientY,
-      };
-    }
-  }
-
-  private onTouchEnd() {
-    this.isDragging = false;
-  }
-
-  private onMouseWheel(
-    event: WheelEvent,
-    renderer: THREE.WebGLRenderer,
-    scene: THREE.Scene
-  ) {
-    // Adjust camera zoom based on scroll direction
-    if (event.deltaY < 0) {
-      this.camera.position.z = Math.max(1, this.camera.position.z - 0.5); // Zoom in
-    } else {
-      this.camera.position.z = Math.min(10, this.camera.position.z + 0.5); // Zoom out
-    }
-
-    renderer.render(scene, this.camera);
+  stopRotation() {
+    this.rotateDirection = null;
   }
 }
