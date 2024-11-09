9b5b1dc6f6ac884f1b26cdbc6003cfb7f0761cec
Sol Andrade <solnicandrade@gmail.com>
2024-11-09 23:03:47 +0200

    responsiv

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
-import { faBell, faSquareArrowUpRight, faPenRuler, faBuilding, faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { Component, OnInit } from '@angular/core';
+import {
+  faBell,
+  faSquareArrowUpRight,
+  faPenRuler,
+  faBuilding,
+  faLocationDot,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css', './itinerary.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   faBell = faBell;
   faSquareArrowUpRight = faSquareArrowUpRight;
   faPenRuler = faPenRuler;
   faBuilding = faBuilding;
   faLocationDot = faLocationDot;
+  isMobile: boolean = true;
+
+  ngOnInit() {
+    this.isMobile = window.innerWidth <= 500;
+
+    window.addEventListener('resize', () => {
+      this.isMobile = window.innerWidth <= 500;
+    });
+  }
 }
